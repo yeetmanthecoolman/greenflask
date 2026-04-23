2b3a9b260a9fd3e7debe47e65779d326a840b45f
The Guy With The Bad Username <75697942+yeetmanthecoolman@users.noreply.github.com>
2026-04-23 13:34:56 -0500

    Add copyright and license information to wsgi.py
    Added licensing information and copyright notice to wsgi.py.

--- src/greenflask/wsgi.py
+++ src/greenflask/wsgi.py
@@ -1,3 +1,19 @@
+#    The main file of the Greenflask programme.
+#    Copyright (C) 2026 Frank J. Barth
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from flask import Flask, Blueprint, render_template
 from greenhaus.main import do_shutdown, change_setting, water, light, camera_capture, create_video
 
